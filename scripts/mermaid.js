53de73d1f1170dc8412b4ec89118202ab7617b2f
SamueleFacenda <samuele.facenda@gmail.com>
2025-10-02 11:16:09 +0200

    vault backup auto nixos: 2025-10-02 11:16:08

--- scripts/mermaid.js
+++ scripts/mermaid.js
@@ -4,5 +4,5 @@
 
   mermaid.registerLayoutLoaders(elkLayouts);
   window.mermaid = mermaid;
-  console.log("Mermaid with ELK layouts loaded!");
+  console.log("✅ Mermaid with ELK layouts loaded!");
 })();
